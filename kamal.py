fab593a9f8dd32d8643b7e677afe090b80b04a13
mosuloiodui <mohamed.elh12345@gmail.com>
2025-04-18 17:32:16 +0100

    Update kamal.py

--- kamal.py
+++ kamal.py
@@ -212,6 +212,9 @@ else:
     st.session_state.df = df
     str='Original'
 st.title("Ransomaware vs Goodware")
+go_model = st.sidebar.button("🚀 Go (Exécuter Modèle)")
+if 'go_model' not in st.session_state:
+    st.session_state.go_model=False
 
 if dataset_choice:
   if go_data:
