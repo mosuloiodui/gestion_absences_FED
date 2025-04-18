8bd84bb89ff5dbe8ce36b30458ee65a656bfe444
mosuloiodui <mohamed.elh12345@gmail.com>
2025-04-18 19:49:26 +0100

    Update kamal.py

--- kamal.py
+++ kamal.py
@@ -88,7 +88,7 @@ def modx(df_original):
     st.sidebar.write("Chargement :",str)
 
     if True:
-        with st.spinner(f"⏳ Entraînement du modèle {name} en cours..."):
+     with st.spinner(f"⏳ Entraînement du modèle {name} en cours..."):
             model.fit(X_train, y_train)
             y_pred = model.predict(X_test)
             y_proba = model.predict_proba(X_test)[:, 1] if hasattr(model, "predict_proba") else None
@@ -156,7 +156,7 @@ def modx(df_tave):
               str=str+" et "
     st.sidebar.write("Chargement :",str)
     if True:
-        with st.spinner(f"⏳ Entraînement du modèle {name} en cours..."):
+       with st.spinner(f"⏳ Entraînement du modèle {name} en cours..."):
             model.fit(X_train, y_train)
             y_pred = model.predict(X_test)
             y_proba = model.predict_proba(X_test)[:, 1] if hasattr(model, "predict_proba") else None
